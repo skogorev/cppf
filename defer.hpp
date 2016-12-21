d50572c1e99d9e8a11afbf176f091b7a400e0e06
Anton Skogorev <a.skogorev@corp.mail.ru>
2016-12-21 12:13:10 +0300

    Fixed salt concat for defer macro.

--- defer.hpp
+++ defer.hpp
@@ -2,38 +2,45 @@
 #define CPPF_DEFER_H
 #include <functional>
 
+//  https://github.com/skogorev/cppf#defer
+
 namespace cppf {
+  namespace aux {
+    class __Defer {
+    public:
+      __Defer(const std::function<void()>& defer_function)
+        : f_(defer_function) {
+      }
+      __Defer(std::function<void()>&& defer_function)
+        : f_(std::forward<std::function<void()>>(defer_function)) {
+      }
+      template<class FN, class... ARGS>
+      __Defer(FN&& f, ARGS&&... args)
+        : f_(std::bind(std::forward<FN>(f), std::forward<ARGS>(args)...)) {
+      }
+
+      __Defer(const __Defer&) = delete;
+      __Defer& operator=(const __Defer&) = delete;
+
+      ~__Defer() {
+        f_();
+      }
+
+    private:
+      std::function<void()> f_;
+    };
+  } // namespace aux
+} // namespace cppf
 
 #ifdef __COUNTER__
-#define __DEFER_SALT __COUNTER__
+#define __CPPF_DEFER_SALT __COUNTER__
 #else
-#define __DEFER_SALT __LINE__
+#define __CPPF_DEFER_SALT __LINE__
 #endif
 
-class __Defer {
-public:
-  __Defer(const std::function<void()>& defer_function)
-    : f_(defer_function) {
-  }
+#define __CPPF_DEFER_CONCAT(x,y) x##y
+#define __CPPF_DEFER_VARIABLE_NAME(x,y) __CPPF_DEFER_CONCAT(x,y)
 
-  __Defer(const __Defer&) = delete;
-  __Defer& operator=(const __Defer&) = delete;
-
-  template<class FN, class... ARGS>
-  __Defer(const FN& f, ARGS... args)
-    : __Defer(std::function<void()>(std::bind(f, args...))) {
-  }
-
-  ~__Defer() {
-    f_();
-  }
-
-private:
-  std::function<void()> f_;
-};
-
-#define CPPF_DEFER(...) cppf::__Defer defer_obj_##__DEFER_SALT(__VA_ARGS__)
-
-} // namespace cppf
+#define CPPF_DEFER(...) cppf::aux::__Defer __CPPF_DEFER_VARIABLE_NAME(defer_obj_, __CPPF_DEFER_SALT)(__VA_ARGS__)
 
 #endif // CPPF_DEFER_H
